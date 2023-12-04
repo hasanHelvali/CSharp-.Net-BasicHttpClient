d14c3861084d276b382671bb3b4397ad58f184ff
HasanHelvali <hasan.helvali.git@gmail.com>
2023-12-04 23:06:09 +0300

    Init

---
+++ BasicHttpRequest_API/Controllers/HttpGet.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Newtonsoft.Json;
+
+namespace BasicHttpRequest_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HttpGet : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            string apiUrl = "https://jsonplaceholder.typicode.com/posts";
+            List<ResponseModel> data = HttpHelper.GetDataFromApi<List<ResponseModel>>(apiUrl).Result;
+            return Ok(data);
+        }
+    }
+    public class HttpHelper
+    {
+        public static async Task<T> GetDataFromApi<T>(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                var result = await client.GetAsync(url);
+                result.EnsureSuccessStatusCode();
+                string resultContentString = await result.Content.ReadAsStringAsync();
+                T resultContent = JsonConvert.DeserializeObject<T>(resultContentString);
+                return resultContent;
+            }
+        }
+    }
+
+    public class ResponseModel
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ BasicHttpRequest_Console/Program.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+
+namespace BasicHttpRequest_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string apiUrl = "https://jsonplaceholder.typicode.com/posts";
+
+            var data = HttpHelper.GetDataFromApi<List<ResponseModel>>(apiUrl).Result;
+
+            foreach (var post in data)
+            {
+                Console.WriteLine("---------------------------");
+                Console.WriteLine(post.Id);
+                Console.WriteLine(post.UserId);
+                Console.WriteLine(post.Title);
+                Console.WriteLine(post.Body);
+                Console.WriteLine("---------------------------");
+            }
+            Console.Read();
+        }
+    }
+    public class HttpHelper
+    {
+        public static async Task<T> GetDataFromApi<T>(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                var result = await client.GetAsync(url);
+                result.EnsureSuccessStatusCode();
+                string resultContentString = await result.Content.ReadAsStringAsync();
+                T resultContent = JsonConvert.DeserializeObject<T>(resultContentString);
+                return resultContent;
+            }
+        }
+    }
+
+    public class ResponseModel
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
